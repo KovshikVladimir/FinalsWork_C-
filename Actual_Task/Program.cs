d8383a1c8e7ab99caf81bdc40f1f4faaeeee4f40
KovshikVladimir <SwidishOne@gmail.com>
2022-07-26 16:42:13 +0300

    FindThreeDigitElements method added

--- Actual_Task/Program.cs
+++ Actual_Task/Program.cs
@@ -1,4 +1,27 @@
-﻿string[] mainArray = { "45", "thanks", "for", "777", "knowledge" };
+﻿void FindThreeDigitElements(string[] inputArray, int countDigit)
+{
+    string[] outputArray = new string[countDigit];
+    int i = 0;
+    int k = 0;
+    Console.Write("[ ");
+    while (i < inputArray.Length)
+    {
+        if (inputArray[i].Length <= 3)
+        {
+            outputArray[k] = inputArray[i];
+            Console.Write(outputArray[k] + " ");
+            i = i + 1;
+            k = k + 1;
+        }
+        else
+        {
+            i = i + 1;
+        }
+    }
+    Console.Write("]");
+}
+
+string[] mainArray = { "45", "thanks", "for", "777", "knowledge" };
 int count = 0;
 
 for (int i = count; i < mainArray.Length; i++)
@@ -8,3 +31,5 @@ for (int i = count; i < mainArray.Length; i++)
         count++;
     }
 }
+
+FindThreeDigitElements(mainArray, count);
