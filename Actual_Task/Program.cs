1a0b87f48e0e58787fbd46b1817fe521e8e4a936
KovshikVladimir <SwidishOne@gmail.com>
2022-07-26 16:41:03 +0300

    counted outputArray Length

--- Actual_Task/Program.cs
+++ Actual_Task/Program.cs
@@ -1,2 +1,10 @@
 ﻿string[] mainArray = { "45", "thanks", "for", "777", "knowledge" };
 int count = 0;
+
+for (int i = count; i < mainArray.Length; i++)
+{
+    if (mainArray[i].Length <= 3)
+    {
+        count++;
+    }
+}
